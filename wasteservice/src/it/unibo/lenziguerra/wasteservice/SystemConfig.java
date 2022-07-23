71f6de89fe15334baf8f9f224a113943463bf7e3
lnwor <43646324+lnwor@users.noreply.github.com>
2022-07-23 15:36:30 +0200

    changed port

--- wasteservice/src/it/unibo/lenziguerra/wasteservice/SystemConfig.java
+++ wasteservice/src/it/unibo/lenziguerra/wasteservice/SystemConfig.java
@@ -3,5 +3,5 @@ package it.unibo.lenziguerra.wasteservice;
 public class SystemConfig {
     public static boolean debugPrint = true;
     public static String storageHost = "localhost";
-    public static int storagePort = 8020;
+    public static int storagePort = 8021;
 }
