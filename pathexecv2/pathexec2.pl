52ced37e9281a76286579cfac020ebe388c7ae92
Filloax <filloax@gmail.com>
2022-08-01 16:58:01 +0200

    init pathexec2

--- pathexecv2/pathexec2.pl
+++ pathexecv2/pathexec2.pl
@@ -1,3 +1,7 @@
 %====================================================================================
 % pathexec2 description   
 %====================================================================================
+context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
+context(ctxpathexec2, "localhost",  "TCP", "8040").
+ qactor( basicrobot, ctxbasicrobot, "external").
+  qactor( pathexec2, ctxpathexec2, "it.unibo.pathexec2.Pathexec2").
