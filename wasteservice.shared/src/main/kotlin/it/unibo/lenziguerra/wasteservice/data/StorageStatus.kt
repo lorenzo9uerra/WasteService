4453fa3c81e98d0a25c4f0526c6cbbe3e8484027
Filloax <filloax@gmail.com>
2022-08-02 16:24:48 +0200

    classe dati  StorageStatus

---
+++ wasteservice.shared/src/main/kotlin/it/unibo/lenziguerra/wasteservice/data/StorageStatus.kt
@@ -0,0 +1,26 @@
+package it.unibo.lenziguerra.wasteservice.data
+
+import it.unibo.lenziguerra.wasteservice.WasteType
+import it.unibo.lenziguerra.wasteservice.utils.PrologUtils
+
+data class StorageStatus (val amounts: Map<WasteType, Float>, val maxAmounts: Map<WasteType, Float>) {
+    companion object {
+        fun fromProlog(prolStr: String): StorageStatus {
+            val amounts = mutableMapOf<WasteType, Float>()
+            val maxAmounts = mutableMapOf<WasteType, Float>()
+            val contentLines = PrologUtils.getFuncLines(prolStr, "content")
+
+            for (line in contentLines) {
+                val payloadArgs = PrologUtils.extractPayload(line)
+                val typeStr = payloadArgs[0]
+                val amount = payloadArgs[1].toFloat()
+                val maxAmount = payloadArgs[2].toFloat()
+                val type = WasteType.values().find { it.id == typeStr } ?: throw IllegalArgumentException("No such waste type $typeStr")
+                amounts[type] = amount
+                maxAmounts[type] = maxAmount
+            }
+
+            return StorageStatus(amounts, maxAmounts)
+        }
+    }
+}
