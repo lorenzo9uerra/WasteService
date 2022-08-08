ad7c9fdf9f11d6ae10b80a59b2754248e1d717b0
Filloax <filloax@gmail.com>
2022-08-08 14:22:11 +0200

    Modello del problema di led

--- model.requisiti/wasteservice_req_deposit.pl
+++ model.requisiti/wasteservice_req_deposit.pl
@@ -2,6 +2,6 @@
 % wasteservice_req_deposit description   
 %====================================================================================
 context(ctxreq_deposit, "localhost",  "TCP", "8050").
- qactor( dep_trolley, ctxreq_deposit, "it.unibo.dep_trolley.Dep_trolley").
-  qactor( dep_waste_boxes, ctxreq_deposit, "it.unibo.dep_waste_boxes.Dep_waste_boxes").
-  qactor( dep_init, ctxreq_deposit, "it.unibo.dep_init.Dep_init").
+ qactor( depositinit, ctxreq_deposit, "it.unibo.depositinit.Depositinit").
+  qactor( trolley_dep, ctxreq_deposit, "it.unibo.trolley_dep.Trolley_dep").
+  qactor( waste_boxes, ctxreq_deposit, "it.unibo.waste_boxes.Waste_boxes").
