6726d9562fb7ca78b26a199568b16ae982d42525
lnwor <43646324+lnwor@users.noreply.github.com>
2022-08-25 18:24:12 +0200

    removed pathexecv2

--- pathexecv2/pathexec2.pl
+++
@@ -1,7 +0,0 @@
-%====================================================================================
-% pathexec2 description   
-%====================================================================================
-context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
-context(ctxpathexec2, "localhost",  "TCP", "8040").
- qactor( basicrobot, ctxbasicrobot, "external").
-  qactor( pathexec2, ctxpathexec2, "it.unibo.pathexec2.Pathexec2").
