861b13424c53926043ef23b56e6c89bc8f917659
lnwor <43646324+lnwor@users.noreply.github.com>
2022-08-25 18:18:29 +0200

    removed unnecessary files

--- wasteservice.core/storagemanager.pl
+++
@@ -1,5 +0,0 @@
-%====================================================================================
-% storagemanager description   
-%====================================================================================
-context(ctx_storagemanager, "localhost",  "TCP", "8021").
- qactor( storagemanager, ctx_storagemanager, "it.unibo.storagemanager.Storagemanager").
