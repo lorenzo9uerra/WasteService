4184f6a82a843d65ff47cd9330b750bbea2597fe
Filloax <filloax@gmail.com>
2022-08-27 15:03:26 +0200

    update a dockerfile vari

---
+++ wasteservice.core/dockerresources/wasteservice_storage.pl
@@ -0,0 +1,12 @@
+%====================================================================================
+% wasteservice description   
+%====================================================================================
+context(ctx_wasteservice, "wasteservice.core",  "TCP", "8023").
+context(ctx_trolley, "wasteservice.trolley",  "TCP", "8070").
+context(ctx_storagemanager, "localhost",  "TCP", "8071").
+context(ctx_pathexecstop, "unibo.pathexecstop",  "TCP", "8025").
+ qactor( pathexecstop, ctx_pathexecstop, "external").
+  qactor( wasteservice, ctx_wasteservice, "it.unibo.wasteservice.Wasteservice").
+  qactor( trolley, ctx_trolley, "it.unibo.trolley.Trolley").
+  qactor( storagemanager, ctx_storagemanager, "it.unibo.storagemanager.Storagemanager").
+  qactor( sonarinterrupter, ctx_trolley, "it.unibo.sonarinterrupter.Sonarinterrupter").
